be92dfb0207b7d3dd8111293969edcd797d46a48
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-12-31 11:35:48 +0700

    Fix code as per recommendation

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -3,7 +3,7 @@ package twofer
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
 	if name == "" {
-		return "One for you, one for me."
+		name = "you"
 	}
 
 	return "One for " + name + ", one for me."
