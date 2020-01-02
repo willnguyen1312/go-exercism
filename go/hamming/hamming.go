9aacb0546a01e1b08c6923d53c1b89e5e46affd4
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-01-02 10:11:24 +0700

    Fix Hamming Distance as per recommendation

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,12 +6,12 @@ import (
 
 // Distance function calculate the Hamming Distance between two DNA strands
 func Distance(a, b string) (int, error) {
-	var result int
 
 	if len(a) != len(b) {
-		return result, errors.New("Invalid input")
+		return 0, errors.New("invalid input")
 	}
 
+	var result int
 	for index := 0; index < len(a); index++ {
 		if a[index] != b[index] {
 			result++
