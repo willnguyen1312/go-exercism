23b3cd7afcf6e19267612f3ccd0219d34d570e7c
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-01-02 11:15:46 +0700

    Roman Numerals

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,41 @@
+package romannumerals
+
+import "errors"
+
+const testVersion = 3
+
+var conversions = []struct {
+	arabic int
+	roman  string
+}{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ToRomanNumeral function
+func ToRomanNumeral(number int) (string, error) {
+	if number <= 0 || number > 3000 {
+		return "", errors.New("number is either too small or too large")
+	}
+
+	result := ""
+	for _, conversion := range conversions {
+		for number >= conversion.arabic {
+			result += conversion.roman
+			number -= conversion.arabic
+		}
+	}
+
+	return result, nil
+}
