9de774a9d3d3d1f7820fbaba91fac14ba397c968
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-01-07 11:28:23 +0700

    Improve Scrabble Score as per mentor's recommendation

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,36 +1,34 @@
 package scrabble
 
-import (
-	"strings"
-)
+import "unicode"
 
-var scrabbleScoreMap = map[string]int{
-	"A": 1,
-	"E": 1,
-	"I": 1,
-	"O": 1,
-	"U": 1,
-	"L": 1,
-	"N": 1,
-	"R": 1,
-	"S": 1,
-	"T": 1,
-	"D": 2,
-	"G": 2,
-	"B": 3,
-	"C": 3,
-	"M": 3,
-	"P": 3,
-	"F": 4,
-	"H": 4,
-	"V": 4,
-	"W": 4,
-	"Y": 4,
-	"K": 5,
-	"J": 8,
-	"X": 8,
-	"Q": 10,
-	"Z": 10,
+var scrabbleScoreMap = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
 }
 
 // Score function calculate the Scrabble score for the word
@@ -38,7 +36,7 @@ func Score(input string) int {
 	var result int
 
 	for _, letter := range input {
-		result += scrabbleScoreMap[strings.ToUpper(string(letter))]
+		result += scrabbleScoreMap[unicode.ToUpper(letter)]
 	}
 
 	return result
